c9df71374a7fc017dcaafbd8d122a6fb101d9ed6
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2024-03-09 18:47:57 +0000

    sign up modal window

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+// Selects all the elements
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn-close-modal");
+const btnOpenModal = document.querySelectorAll(".btn-show-modal");
+
+//////////////////////////////////////////////////
+// MODAL WINDOW
+// Opens modal window
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// Closes modal window
+const closeModal = function (e) {
+  e.preventDefault();
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// Closes modal window when key down
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
